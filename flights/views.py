b58191b6f43e77f9c208259e9c9c6a16a409a3d9
Maan17 <47431580+Maan17@users.noreply.github.com>
2020-09-03 08:45:41 +0530

    Add passenger,login,logout features

--- flights/views.py
+++ flights/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.http import Http404
-from .models import Flight
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from .models import Flight,Passenger
 # Create your views here.
 def index(request):
     return render(request,"flights/index.html",{
@@ -11,5 +12,12 @@ def flight(request,flight_id):
     flight=Flight.objects.get(pk=flight_id)
     return render(request,"flights/flight.html",{
         "flight":flight,
-        "passengers":flight.passengers.all()
+        "passengers":flight.passengers.all(),
+        "non_passengers":Passenger.objects.exclude(flights=flight).all()
     })
+def book(request,flight_id):
+    if request.method=="POST":
+        flight=Flight.objects.get(pk=flight_id)
+        passenger=Passenger.objects.get(pk=int(request.POST["passenger"]))
+        passenger.flights.add(flight)
+        return HttpResponseRedirect(reverse("flight",args=(flight.id,)))
